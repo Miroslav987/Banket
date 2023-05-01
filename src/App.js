000bada9c998d60b91d6a3f62d9b68a3875edb66
User <user@Users-MacBook-Air.local>
2023-05-01 23:33:57 +0600

    added add-product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Mainrouter from "./Mainrouter";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Mainrouter/>
+    </div>
+  )
 };
 
 export default App;
--- src/Mainrouter.js
+++ src/Mainrouter.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AddRst from "./components/AddRst/AddRst";
+import Navbar from "./components/Navbar/Navbar";
 
 const Mainrouter = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+        <Route path="/" element={<AddRst/>} />
+        <Route path="/nav" element={<Navbar/>} />
+   </Routes>
+  )
+    
 };
 
 export default Mainrouter;
